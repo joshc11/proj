5f8166358266feae866fac5d4f835de28d2f010d
Joshua Chongo <jchongo1@umbc.edu>
2022-10-19 14:51:48 -0400

    free me

--- LikeServer.c
+++ LikeServer.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 int main (int argc, char **argv){
-	char* name = argv[1];
+	char* name = argv[0];
 	printf("This is %s\n", name);
 }
--- ParentProcess.c
+++ ParentProcess.c
@@ -9,22 +9,24 @@ const char* ERROR_STRING = "Child Server failed to start";
 const char* FILE_NAME = "./LikeServer";
 const int CHILD = 0;
 const int ERROR = 1;
-
+const int BUFFER = 30;
 int main(){
-	char name[100];
-	char* number;
+	char name[BUFFER];
+	static char * args[1];
+	char number[BUFFER];
 	pid_t pid;
 	for (int i = 1; i <= SERVER_COUNT; i++){
 		pid = fork();
-		*number = (char)i;
+		sprintf(number, "%d", i);
 		strcpy(name, SERVER);
 		strcat(name, number);
+		args[0] = name;
 		if (pid < CHILD) {
 			fprintf(stderr, ERROR_STRING);
 			return ERROR;
 		}
 		if (pid == CHILD){
-			execvp(FILE_NAME, name);
+			execvp(FILE_NAME, args);
 		}
 	}
 	return 0;
