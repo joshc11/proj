b65f4a998b340faf56d29d0f62b6fd5b47f6ea34
Joshua Chongo <jchongo1@umbc.edu>
2022-10-22 17:43:59 -0400

    we

--- LikeServer.c
+++ LikeServer.c
@@ -8,11 +8,12 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
-const float RUNTIME = 5 * 60;
+const float RUNTIME = .5 * 60;
 const char *APPEND = "a";
 const char *DIRECTORY = "/tmp/";
 const int BUFFER = 100;
 const int PORT = 9000;
+
 float elasped(struct timeval start){
 	struct timeval current;
 	gettimeofday(&current, NULL);
@@ -20,17 +21,18 @@ float elasped(struct timeval start){
 }
 
 int main (int argc, char **argv){
-	int server;
-	server = socket(AF_INET, SOCK_STREAM, 0);
+	int remote, server;
+	if((server = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		abort();
+	};
 	struct sockaddr_in address;
 	address.sin_family = AF_INET;
 	address.sin_port = htons(PORT);
 	address.sin_addr.s_addr = INADDR_ANY;
-	bind(server, (struct sockaddr*) &address, sizeof(address));
-	listen(server, 10);
-	int client = accept(server, NULL, NULL);
-
-	char* name = argv[0];
+	if ((remote = connect(server, (struct sockaddr*) &address, sizeof(address))) == -1){
+		abort();
+	};
+	char* name = argv[1];
 	int count;
 	char message[BUFFER];
 	FILE *log;
@@ -40,15 +42,18 @@ int main (int argc, char **argv){
 	struct timeval start;
 	gettimeofday(&start, NULL);
 	srand((int)start.tv_sec);
+
 	while (elasped(start) < RUNTIME){
 		count = rand() % 43;
 		sprintf(message,"%s %d", name, count);
-		send(client, message, sizeof(message),0);
+		send(server, message, sizeof(message),0);
+		sprintf(message,"1");
+		recv(remote, &message, sizeof(message), 0);
 		log = fopen(fileName, APPEND);
-		fprintf(log, "%s %d\n", name, count);
+		fprintf(log, "Likeserver %d %s\n", count, message);
 		fclose(log);
 		sleep(rand() % 5 + 1);
 	}
-	close(server);
+	close(remote);
 	return 0;
 }
--- PrimaryLikesServer.c
+++ PrimaryLikesServer.c
@@ -4,18 +4,39 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
+const int PORT = 9000;
+const int BUFFER = 100;
+const int SERVER_COUNT = 10;
+const char *LOG_FILE = "/tmp/PrimaryLikesLog";
+const char *DELIMITER = " ";
+const char *SUCCESS = "0";
 int main (){
-	int network;
-	network = socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in server;
-	server.sin_family = AF_INET;
-	server.sin_port = htons(9002);
-	server.sin_addr.s_addr = INADDR_ANY;
-	int status = connect(network, (struct sockaddr *) &server, sizeof(server));
-	if (status == 1){printf("funy");}
-	char message[100];
-	recv(network, &message, sizeof(message), 0);
-	printf("%s", message);
-	close(network);
+	FILE *log;
+	char* client_name;
+	char* count_string;
+	int count;
+	int total = 0;
+	int network = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in address;
+	address.sin_family = AF_INET;
+	address.sin_port = htons(PORT);
+	address.sin_addr.s_addr = INADDR_ANY;
+	bind(network, (struct sockaddr*) &address, sizeof(address));
+	listen(network, SERVER_COUNT);
+	char message[BUFFER];
+	int client = accept(network, NULL, NULL);
+	//while(recv(client, &message, sizeof(message),0)!=0){
+		recv(client, &message, sizeof(message),0);
+//		send(network, SUCCESS, sizeof(SUCCESS), 0);
+		client_name = strtok(message, DELIMITER);
+		count_string = strtok(NULL, DELIMITER);
+		count = atoi(count_string);
+		total += count;
+		printf("%s %d\n", client_name, count);
+		printf("Total   %d\n", total);
+//	}
+	close(client);
+	shutdown(network, SHUT_RDWR);
 	return 0;
 }
